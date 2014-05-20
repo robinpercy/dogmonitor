3691c8810e5a0e2f1aea9469ed5839d2e2bc6d30
Robin Percy <rpercy@gmail.com>
2014-05-19 19:34:00 -0700

    A bit of cleanup

--- monitor.py
+++ monitor.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
+
 """
-PyAudio example: Record a few seconds of audio and save to a WAVE
-file.
+The goal of this script is to provide positive-reinforcement for dogs when they're left alone.  It will monitor the average volume in a room and assumes that any noise above a specific threshold is barking.
+
+When barking starts, it plays a wav file that corrects the dogs.  After a brief period of silence, it plays a wav file that encourages the dogs.  Ultimately, the logic for both of these will involve exponential back-offs.
+
+TODO:
+    * Add an exponential back off to the "good dog" wav file
+    * Start communicating the "correct" and "encourage" events to an arduino (so we can release treats as part of encouragement)
+    * Monitor external factors that may correlate and predict barking so we can pre-empt it with positive feedback.  Potential variables could be: average volume in room (indicating noise from outside), time of day, temperature in room.
 """
 
 import pyaudio
@@ -9,7 +16,6 @@ import wave
 import sys
 import time
 from array import array
-
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -19,86 +25,97 @@ RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 QUIET_FILENAME = "quiet.wav"
 GOOD_DOG_FILENAME = "good_dogs.wav"
-PLAYING = False
-
-last_noise = time.time()
+QUIET = False
+NOISY = True
+THRESHOLD = 500
 
 if sys.platform == 'darwin':
     CHANNELS = 1
 
+
+"""
+Initialize the two wave files we'll use for correction and encouragement
+"""
+quiet_wav = wave.open(QUIET_FILENAME)  
+good_dog_wav = wave.open(GOOD_DOG_FILENAME)
+
+"""
+Setup a full duplex stream using default devices
+"""
 p = pyaudio.PyAudio()
 
-input_stream = p.open(format=FORMAT,
+stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 output=True,
                 frames_per_buffer=CHUNK)
 
-quiet_wav = wave.open(QUIET_FILENAME)  
-good_dog_wav = wave.open(GOOD_DOG_FILENAME)
-
+last_noise = time.time()
 data_out = ""
-
 frames = array('h')
-
-"""
-def say(wav, CHUNK):
-
-    wav.rewind()
-    return quiet_wav.readframes(CHUNK)
-    """
-
-QUIET = False
-NOISY = True
 sound_was = QUIET
 sound_is = QUIET
-i = 0;
+
+def avg_volume(stream):
+    global CHUNK, THRESHOLD
+    data = array('h',stream.read(CHUNK))
+    return sum([abs(i) for i in data]) / len(data)
+
+def is_noisy(volume):
+    return volume > THRESHOLD
+
+"""
+Loop until the user hits Ctrl+C. In the loop, we analyze the latest buffer from pyaudio then decide whether or not
+to initiate playing a wav file.
+
+When playing a wav file, we only write out one CHUNK of buffer per iteration.  Otherwise, pyaudio complains of a buffer overflow when we try to read from the input buffer. So, this means the loop is either in one of three states:
+    1. Not Playing Wave File
+    2. Playing "Good Dog" Wave File
+    3. Playing "Be Quiet" Wave file
+
+The states are determined by the wf and data_out variables.  If data_out contains data, then a file is being played and wf will be pointed at the file being played.  Note that we're careful not to initiate playing a file if one is already being played.
+"""
 try: 
     while True:
         sound_was = sound_is 
-        data = array('h',input_stream.read(CHUNK))
-        avg = sum([abs(i) for i in data]) / len(data)
-        sound_is = avg > 500 
-        sys.stdout.write("                                         ")
+        volume = avg_volume(stream)
+        sound_is = is_noisy(volume)
+
         if sound_is == NOISY:
             last_noise = time.time()
 
-        quiet_since = time.time() - last_noise
-        notice = ""
-        if sound_was == NOISY and sound_is == QUIET and data_out == "": 
-            notice = "All is calm" 
-            wf = quiet_wav
-            wf.rewind()
-            data_out = wf.readframes(CHUNK)
-        elif data_out == "" and quiet_since > 3.0:
-            wf = good_dog_wav
-            wf.rewind()
-            data_out = wf.readframes(CHUNK)
-            #data_out = say(good_dog_wav, CHUNK)
+        seconds_quiet = time.time() - last_noise
 
+        """
+        If we don't have output queued up, check if we should start playing one of the wav files
+        """
+        if data_out == "":
+            if sound_was == QUIET and sound_is == NOISY: 
+                wf = quiet_wav
+                wf.rewind()
+                data_out = wf.readframes(CHUNK)
+            elif seconds_quiet > 3.0:
+                wf = good_dog_wav
+                wf.rewind()
+                data_out = wf.readframes(CHUNK)
 
+        """
+        If we have output queued up, play the next chunk of it
+        """
         if data_out != "":
-            input_stream.write(data_out)
+            stream.write(data_out)
             data_out = wf.readframes(CHUNK)
 
-        sys.stdout.write("\rNoise level: %s : %s " % (avg, quiet_since))
+        sys.stdout.write("                                         ")
+        sys.stdout.write("\rVolume: %s : %s " % (volume, seconds_quiet))
         sys.stdout.flush()
-        #frames.extend(data)
         
 except (KeyboardInterrupt):
     print("Exiting")
 
 
-input_stream.stop_stream()
-input_stream.close()
+stream.stop_stream()
+stream.close()
 p.terminate()
 
-"""
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-"""
